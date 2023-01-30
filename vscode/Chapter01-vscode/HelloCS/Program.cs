44306ed33e695881eac7ebc210c96285fb0a6eed
andierrrr <allaican@kean.edu>
2023-01-30 00:02:21 -0500

    first testing

--- vscode/Chapter01-vscode/HelloCS/Program.cs
+++ vscode/Chapter01-vscode/HelloCS/Program.cs
@@ -1,4 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, C#!");
-throw new Exception();
-int z;
+//throw new Exception()
+int myNum = 15;
+myNum = 20; // myNum is now 20
+Console.WriteLine(myNum);
+Console.WriteLine("my name is Andy Allaico"); 
+Console.WriteLine("Version: {0}", Environment.Version.ToString()); 
